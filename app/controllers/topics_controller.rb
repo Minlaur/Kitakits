088da56063f9f1daa6e815624b4354435b333341
Laurice Minamihara <lauricejoie.m@gmail.com>
2024-02-10 14:32:58 +0900

    Updates the topic controller and add strong params

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,7 +1,25 @@
 class TopicsController < ApplicationController
+
+  def new
+    @topic = Topic.new
+  end
+
   def create
+    @topic = Topic.new(topic_params)
+    if @topic.save
+      redirect_to topics_path
+      # should update, redirect to the recommended sempais path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
+  end
+
+  private
+
+  def topic_params
+    params.require(:topic).permit(:name, :description)
   end
 end
