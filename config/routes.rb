adfe9cd0654c8501e4537f511d019132eeb6b468
portal martin <ptl_martin@yahoo.co.jp>
2024-02-10 15:51:09 +0900

    refine the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  root to: "page#home"
 
-  # resources :tags do
-  #   resources :topics, only: [:index]
-  # end
   namespace :sempais do
     get '/sempais', to: 'sempais#index'
-    get '/sempais/:id', to: 'sempais#show', as: 'sempai'
+    get '/sempais/:id', to: 'sempais#show'
+    get '/dashboard', to: 'sempais#dashboard'
+    get '/bookings', to: 'sempais#bookings'
+    get '/bookings/:id', to: 'sempais#booking'
   end
 
   resources :topics, except: [:index, :destroy] do
     resources :bookings, only: [:new, :create]
   end
 
-  resources :bookings, only: [:show, :edit, :update,]
+  resources :bookings, only: [:show, :edit, :update]
 
 end
