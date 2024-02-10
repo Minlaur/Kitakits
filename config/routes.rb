ec14e652e9931aa5cc8eadde99b87cb98be768f1
Laurice Minamihara <lauricejoie.m@gmail.com>
2024-02-10 18:39:19 +0900

    Updates the db

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to: "page#home"
+  root to: "pages#home"
 
   get '/dashboard', to: 'dashboard#show'
 
