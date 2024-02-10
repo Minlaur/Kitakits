29ba1a7df0750a4825f031d0c3e8eed5565eeb9e
portal martin <ptl_martin@yahoo.co.jp>
2024-02-10 16:44:12 +0900

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,18 +3,22 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: "page#home"
 
+  get '/dashboard', to: 'dashboard#show'
+
+  get '/sempais/:id', to: 'users#show'
+
   namespace :sempais do
-    get '/sempais', to: 'sempais#index'
-    get '/sempais/:id', to: 'sempais#show'
-    get '/dashboard', to: 'sempais#dashboard'
-    get '/bookings', to: 'sempais#bookings'
-    get '/bookings/:id', to: 'sempais#booking'
+    get '/bookings', to: 'sempais/bookings#index'
+    get '/bookings/:id', to: 'sempais/bookings#show'
+    patch '/bookings/:id', to: 'sempais/bookings#update'
+
   end
 
   resources :topics, except: [:index, :destroy] do
     resources :bookings, only: [:new, :create]
   end
 
-  resources :bookings, only: [:show, :edit, :update]
-
+  resources :bookings, only: [:index, :show, :edit, :update] do
+    resources :messages, only: [:create]
+  end
 end
