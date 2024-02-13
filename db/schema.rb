19aafedecda2345aeafe7aedcd37cc592e977799
portal martin <ptl_martin@yahoo.co.jp>
2024-02-13 22:35:43 +0900

    addin image to message addings cloudinary association between active storage and model:   has_many_attached :images  for messages   has_one_attached :picture for user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_13_132328) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_13_133255) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,6 +57,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_13_132328) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
     t.index ["booking_id"], name: "index_messages_on_booking_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
