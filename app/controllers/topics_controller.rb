17c8e0693b10213cac91186af301e9ea14b3464c
portal martin <ptl_martin@yahoo.co.jp>
2024-02-18 08:58:39 +0900

    modified:   app/controllers/topics_controller.rb 	modified:   app/controllers/users_controller.rb 	modified:   app/models/user.rb     take out validation for user first_name and last_name

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,12 +1,11 @@
 class TopicsController < ApplicationController
-  skip_before_action :authenticate_user!, only:[:show, :create, :matching_sempais]
 
   def create
     @topic = Topic.new(topic_params)
     @topic.user = current_user
     if @topic.save
       # @topic = Topic.find(params[:topic_id])
-      redirect_to @topic
+      redirect_to topic_path(@topic)
       # should update, redirect to the recommended sempais path
     else
       render 'pages/home', status: :unprocessable_entity
@@ -14,8 +13,12 @@ class TopicsController < ApplicationController
   end
 
   def show
-    # @topic = Topic.find(params[:topic_id])
-    matching_sempais
+   # find the record of topic and sets it to @topic
+   @topic = Topic.find(params[:id])
+   # finds the record of users and sets it as @sempais
+   tags = @topic.name.split(" ") + @topic.description.split(" ")
+   @sempais = User.where(sempai: true).tagged_with(tags, any: true)
+   # @sempais = User.where(sempai: true).tagged_with([@topic.where(params[:query])], on: :expertises, any: true)
   end
 
   def edit
@@ -33,19 +36,19 @@ class TopicsController < ApplicationController
     end
   end
 
-  def matching_sempais
-    # find the record of topic and sets it to @topic
-    @topic = Topic.find(params[:id])
-    # finds the record of users and sets it as @sempais
-    tags = @topic.name.split(" ") + @topic.description.split(" ")
-    @sempais = User.where(sempai: true).tagged_with(tags, any: true)
-    # @sempais = User.where(sempai: true).tagged_with([@topic.where(params[:query])], on: :expertises, any: true)
-  end
+  # def matching_sempais
+  #   # find the record of topic and sets it to @topic
+  #   @topic = Topic.find(params[:id])
+  #   # finds the record of users and sets it as @sempais
+  #   tags = @topic.name.split(" ") + @topic.description.split(" ")
+  #   @sempais = User.where(sempai: true).tagged_with(tags, any: true)
+  #   # @sempais = User.where(sempai: true).tagged_with([@topic.where(params[:query])], on: :expertises, any: true)
+  # end
 
   private
 
   def topic_params
-    params.require(:topic).permit(:name, :description, :tag_id)
+    params.require(:topic).permit(:name, :description)
     # .merge; assuming we can submit a topic if signed in? .merge(user_id: current_user.id, status: "pending")
   end
 end
