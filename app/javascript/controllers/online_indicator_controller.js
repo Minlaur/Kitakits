1a682efbf228ca78c444f16485825494d468488e
Laurice Minamihara <lauricejoie.m@gmail.com>
2024-02-21 09:41:48 +0900

    Set up online indicator stimulus controller

---
+++ app/javascript/controllers/online_indicator_controller.js
@@ -0,0 +1,19 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="online-indicator"
+export default class extends Controller {
+
+static values = {
+  user: Object,
+  userId: Number,
+  lastSeen: ,
+}
+
+  connect() {
+    console.log(this.userIdValue)
+    console.log(this.userValue)
+    console.log(this.lastSeenValue)
+    // console.log(`/users/${currentUserId}/edit`)
+    // console.log(Date.now())
+  }
+}
