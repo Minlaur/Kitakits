088e98dd22333e961c32c686c0e40a47fef40bdb
MomokoJane Kina <momokokina@gmail.com>
2024-02-22 13:12:59 +0900

    working on booking create

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,25 +5,62 @@ def index
 end
 
 # I can display a new booking
+# def new
+#   @booking = Booking.new
+#   @topic = Topic.find(params[:topic_id])
+#   @booking.topic = @topic
+#   # raise
+# end
+
+
+
+
+
 def new
   @booking = Booking.new
   @topic = Topic.find(params[:topic_id])
-  @booking.topic = @topic
-  # raise
-end
-
-# I can create a new booking
-def create
-  @topic = Topic.find(params[:topic_id])
-  # raise
-  @booking = Booking.new(booking_params)
   @booking.topic = @topic
   if @booking.save
     redirect_to booking_path(@booking) # Modify redirect path to include booking ID
   else
     render :new, status: :unprocessable_entity
+   end
+  # raise
+end
+
+
+def update
+  # @booking = Booking.find(params[:id])
+  # if @booking.update(booking_params)
+  #   redirect_to new_topic_booking_path(@booking), notice: "Booking was successfully updated!"
+  # else
+  #   render :new, status: :unprocessable_entity
+  # end
+  if @booking.update
+  redirect_to new_topic_booking_path(@booking), notice: "Booking was successfully updated!"
+  else
+    render :new, status: :unprocessable_entity
   end
 end
+
+
+def create
+  @topic = Topic.find(params[:topic_id])
+  #raise
+  @booking = Booking.new(booking_params)
+  @booking.topic = @topic
+  if @booking.update(booking_params)
+    redirect_to new_topic_booking_path(@booking), notice: "Booking has been confirmed!"
+  elsif @booking.save
+  redirect_to booking_path(@booking) # Modify redirect path to include booking ID
+  else
+    render :new, status: :unprocessable_entity
+   end
+end
+
+
+
+# I can create a new booking
 
 # I can see the details of a booking
 def show
@@ -32,6 +69,8 @@ def show
   @message = Message.new
 end
 # accept or reject the booking by clicking on a button
+
+
 
 
 private
