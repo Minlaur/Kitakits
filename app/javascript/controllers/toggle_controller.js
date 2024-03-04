5e990cb15839c2d5d30ba1ddff62e960a9f51627
portal martin <ptl_martin@yahoo.co.jp>
2024-03-04 20:13:20 +0900

    adding button resolve on sempai page(no functionnal to resolved topic) on click show review button

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,13 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
-  static targets = ["togglableElement"]
+  static targets = ["togglableElement", "secondTogglableElement"]
 
   connect() {
-    console.log("Hello from toggle_controller.js")
 
   }
   fire() {
     this.togglableElementTarget.classList.toggle("d-none");
+    this.secondTogglableElementTarget.classList.toggle("d-none");
   }
 }
